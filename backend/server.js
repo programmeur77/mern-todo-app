66fce9278f346f291dde42240b5fb0ea5d50f7da
programmeur77 <33042825+programmeur77@users.noreply.github.com>
2023-12-18 17:56:31 +0100

    Initialization of npm and installation of first packages in Package.json + Initialization of server

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Header',
+    'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, DELETE, PATCH, OPTIONS'
+  );
+  next();
+});
+
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'Server works !' });
+});
+
+app.listen(process.env.SERVER_PORT);
